9df698f584b2caac4924cc7ee1e04a96723e85bb
Gianluca Bine <gian_bine@hotmail.com>
2020-03-22 18:01:01 -0300

    Scaffold de auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes([
+    'register' => false,
+    'verify' => false,
+    'reset' => true,
+]);
+
+Route::get('/', 'HomeController@index')->name('home.index');
