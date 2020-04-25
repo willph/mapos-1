27508bc5ff43735fe0cc1701df97762f30e29fda
Gianluca Bine <gian_bine@hotmail.com>
2020-04-25 15:49:49 -0300

    Adicionado traduções de mensagens de serviços

--- resources/lang/pt-br/messages.php
+++ resources/lang/pt-br/messages.php
@@ -4,17 +4,25 @@
 
 return [
 
-    'users' => 'Usuários',
+    // General
     'name' => 'Nome',
     'email' => 'Email',
     'password' => 'Senha',
     'repeat_password' => 'Repita a Senha',
     'save' => 'Salvar',
-
+    'confirm' => 'Confirmar',
+    'cancel' => 'Cancelar',
+    'created_at' => 'Criado em',
+    'updated_at' => 'Atualizado em',
+    'go_back' => 'Voltar',
     'view' => 'Visualizar',
     'edit' => 'Editar',
     'delete' => 'Excluir',
+    'price' => 'Preço',
+    'description' => 'Descrição',
 
+    // Users CRUD
+    'users' => 'Usuários',
     'edit_user' => 'Editar Usuário',
     'create_user' => 'Criar Usuário',
     'view_user' => 'Visualizar Usuário',
@@ -22,12 +30,14 @@ return [
     'confirm_user_delete' => 'Confirma a exclusão do usuário?',
     'user_deleted_success' => 'Usuário deletado com sucesso!',
     'user_deleted_fail' => 'Falha ao deletar usuário!',
-    'confirm' => 'Confirmar',
-    'cancel' => 'Cancelar',
 
+    // Services CRUD
+    'services' => 'Serviços',
+    'edit_service' => 'Editar Serviço',
+    'create_service' => 'Criar Serviço',
+    'view_service' => 'Visualizar Serviço',
 
-    'created_at' => 'Criado em',
-    'updated_at' => 'Atualizado em',
-    'go_back' => 'Voltar',
-
+    'confirm_service_delete' => 'Confirma a exclusão do serviço?',
+    'service_deleted_success' => 'Serviço deletado com sucesso!',
+    'service_deleted_fail' => 'Falha ao deletar serviço!',
 ];
